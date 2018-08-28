b0af76558247b0a9a9f3739039e8c3cb9b78c32e
Daniel Koga <daniel.koga@intelectah.com.br>
2018-08-28 19:41:07 -0300

    Criação da classe CommandHandler.

---
+++ Ynode/src/Ynode.Domain/CommandHandler/CommandHandler.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation.Results;
+using System.Threading.Tasks;
+using Ynode.Domain.Core.Bus;
+using Ynode.Domain.Core.Notifications;
+using Ynode.Domain.Interfaces;
+
+namespace Ynode.Domain.CommandHandler
+{
+    public abstract class CommandHandler 
+    {
+        private readonly IUnitOfWork _uow;
+        private readonly IBus _bus;
+        private readonly IDomainNotificationHandler<DomainNotification> _notifications;
+
+        protected CommandHandler(IUnitOfWork uow, IBus bus, IDomainNotificationHandler<DomainNotification> notifications)
+        {
+            _uow = uow;
+            _bus = bus;
+            _notifications = notifications;
+        }
+
+        protected void NotifyValidationError(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                _bus.RaiseEvent(new DomainNotification(error.PropertyName, error.ErrorMessage));
+            }
+        }
+
+        protected bool Commit()
+        {
+            if (_notifications.HasNotifications()) return false;
+            var commandResponse = _uow.Commit();
+            if (commandResponse.Success) return true;
+
+            _bus.RaiseEvent(new DomainNotification("Commit", "Ocorreu um erro ao salvar os dados no banco."));
+            return false;
+        }
+
+        protected async Task<bool> CommitAsync()
+        {
+            if (_notifications.HasNotifications()) return false;
+            var commandResponse = await _uow.CommitAsync();
+            if (commandResponse.Success) return true;
+
+            _bus.RaiseEvent(new DomainNotification("Commit", "Ocorreu um erro ao salvar os dados no banco."));
+            return false;
+        }
+    }
+}
